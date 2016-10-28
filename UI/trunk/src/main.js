3341ce4a8a72460c7db12bd56e59b844ec16ac81
gpimpler <gpimpler@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2016-10-28 18:11:43 +0000

    Renaming poc folder to Explore

--- UI/trunk/src/main.js
+++ UI/trunk/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+Vue.use(VueRouter)
