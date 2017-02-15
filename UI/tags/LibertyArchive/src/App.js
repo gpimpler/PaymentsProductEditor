89646bc263326727a2259cdf5142de317e07813f
Grant Pimpler <gpimpler@grantpimpler.com>
2017-02-15 19:15:05 +0000

    [TAG] Archive of previous Liberty work

---
+++ UI/tags/LibertyArchive/src/App.js
@@ -0,0 +1,10 @@
+import Hello from './components/Hello'
+import './assets/sass/style.scss'
+
+export default {
+  template: require('./App.html'),
+  components: {
+    Hello
+  }
+}
+
---
+++ UI/tags/LibertyArchive/src/components/Hello.js
@@ -0,0 +1,9 @@
+export default {
+  template: require('./templates/Hello.html'),
+  data () {
+    return {
+      msg: 'Hello Vue! from Comp'
+    }
+  }
+}
+
