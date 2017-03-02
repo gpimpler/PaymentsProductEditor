119d71f5f93908310bdf29bae63c7e8dd625d532
gpimpler <gpimpler@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-02 18:53:31 +0000

    Adds top navigation bar

---
+++ UI/trunk/resources/assets/js/components/AppNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="common__navigation">
+        <ul>
+            <li><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Product Designer</li>
+            <li><i class="fa fa-flag" aria-hidden="true"></i> Accents</li>
+            <li><i class="fa fa-cloud-upload" aria-hidden="true"></i> Publisher</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('AppNavigation mounted.')
+        }
+    }
+</script>
