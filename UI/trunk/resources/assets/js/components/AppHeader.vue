243dc6137478279a875f515b109b0a638ac7062b
Grant Pimpler <gpimpler@grantpimpler.com>
2017-03-02 18:53:31 +0000

    Adds top navigation bar

--- UI/trunk/resources/assets/js/components/AppHeader.vue
+++ UI/trunk/resources/assets/js/components/AppHeader.vue
@@ -1,18 +1,26 @@
 <template>
-    <div class="common__header ">
-        <div style="display:inline-block">
-            <img src="../../images/harland-clarke-logo.png" style="height:70px" />
+    <div class="common__header">
+        <div class="common__header-content">
+            <div style="display:inline-block">
+                <img src="../../images/harland-clarke-logo.png" style="height:70px" />
+            </div>
+            <div class="float-right common__user">
+                <span class="common__user-name">User Name</span> <i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+            </div>
         </div>
-        <div class="float-right common__user">
-            <span class="common__user-name">User Name</span> <i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
-        </div>
+        <app-navigation></app-navigation>
     </div>
 </template>
 
 <script>
+    import AppNavigation from './AppNavigation.vue'
+
     export default {
         mounted() {
             console.log('AppHeader mounted.')
+        },
+        components: {
+            AppNavigation
         }
     }
 </script>
