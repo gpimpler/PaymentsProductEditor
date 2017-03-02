17cc3bab52f730ea91f8410a28da431fd42f26d0
gpimpler <gpimpler@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-02 23:11:56 +0000

    Start of the Designer view layout.

--- UI/trunk/resources/assets/js/views/Designer.vue
+++ UI/trunk/resources/assets/js/views/Designer.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="">
-        Designer
+    <div class="row expanded" style="margin:0;padding:0">
+        <div class="large-9 medium-8 columns" style="background-color:#ECECEC;">
+            <configurator></configurator>
+        </div>
+        <div class="large-3 medium-4 columns common__sidebar">
+            Sidebar
+        </div>
     </div>
 </template>
 
 <script>
+    import Configurator from '../components/Configurator.vue';
+
     export default {
         mounted() {
             console.log('Designer mounted.')
+        },
+        components: {
+            Configurator
         }
     }
 </script>
