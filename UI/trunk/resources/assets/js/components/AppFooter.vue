4a29f6077b1df38016bfc1e2649e24ec443d8299
Grant Pimpler <gpimpler@grantpimpler.com>
2017-03-02 14:46:27 +0000

    Add common header and footer components

---
+++ UI/trunk/resources/assets/js/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="common__footer ">
+        Copyright {{year}} Harland Clarke Holdings Inc.
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('AppFooter mounted.')
+        },
+        computed: {
+            year: function () {
+                return "2017"
+            }
+        }
+    }
+</script>
---
+++ UI/trunk/resources/assets/js/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="common__header ">
+        <div style="display:inline-block">
+            <img src="../../images/harland-clarke-logo.png" style="height:70px" />
+        </div>
+        <div class="float-right common__user">
+            <span class="common__user-name">User Name</span> <i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('AppHeader mounted.')
+        }
+    }
+</script>
