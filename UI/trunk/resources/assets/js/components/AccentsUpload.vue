21f863cffe3c608ba95cf7bba900ba197c5da734
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-03 19:47:46 +0000

    adding content for Accents view

---
+++ UI/trunk/resources/assets/js/components/AccentsUpload.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="accents__upload">
+        <div class="accents__upload-nav">
+            <button class="accents__upload-button">Pridemarks/Accents</button>
+            <button class="accents__upload-button">Expression/One-Liner</button>
+            <button class="accents__upload-button">Monogram</button>
+        </div>
+        <div class="accents__upload-area">
+            <h3 class="accents__upload-title">Upload Pridemarks or Accents</h3>
+            <div class="accents__upload-drop">
+                <p>Drag and Drop Images Here</p>
+            </div>
+            <button class="accents__upload-button">Select Files</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            model: build.model
+        }),
+        mounted() {
+            console.log('Accents Upload mounted.');
+        }
+    }
+</script>
