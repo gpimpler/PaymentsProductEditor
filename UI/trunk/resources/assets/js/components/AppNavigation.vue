70e7d63703a7800064b9223093d672adb174b889
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-03 18:52:25 +0000

    fixes gap between navigation items

--- UI/trunk/resources/assets/js/components/AppNavigation.vue
+++ UI/trunk/resources/assets/js/components/AppNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="common__navigation">
+    <div class="common__navigation clearfix">
         <div class="common__navigation-btn" @click="launch('designer')"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Designer</div>
         <div class="common__navigation-btn" @click="launch('accents')"><i class="fa fa-flag" aria-hidden="true"></i> Accents</div>
         <div class="common__navigation-btn" @click="launch('publisher')"><i class="fa fa-cloud-upload" aria-hidden="true"></i> Publisher</div>
