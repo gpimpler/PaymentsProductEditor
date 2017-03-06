47c0c1f59e6e9bd722de0931d38cf5b8462bb918
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-06 22:57:43 +0000

    adds publisher layout styles

--- UI/trunk/resources/assets/js/views/Publisher.vue
+++ UI/trunk/resources/assets/js/views/Publisher.vue
@@ -1,44 +1,75 @@
 <template>
-    <div class="row expanded" style="margin:0;padding:0">
-        <div class="large-9 medium-8 columns">
+    <div class="row expanded publisher" style="margin:0;padding:0">
+        <div class="large-12 medium-12 columns">
             <div class="publisher__introduction">
-                <h1><i class="fa fa-cloud-upload"></i> Publisher</h1>
-                <p>Publisher allows you to update products out to the various environments. You can publish all changes or be selective of those that you want to publish.</p>
+                <div class="publisher__introduction-content">
+                    <h1 class="common__title"><i class="fa fa-cloud-upload"></i> Publisher</h1>
+                    <p>Publisher allows you to update products out to the various environments. You can publish all changes or be selective of those that you want to publish.</p>
+                </div>
+                <div class="publisher__introduction-button">
+                    <div class="publisher__button"><button class="common__button">Publish All</button><span class="publisher__button-count">49</span></div>
+                </div>
             </div>
         </div>
-        <div class="large-3 medium-4 columns">
-            <div><button>Publish All</button><span>49</span></div>
-        </div>
-        <div class="large-6 medium-12 columns">
-            <div class="publisher__upload">
-                <h2>Product Layouts</h2>
-                <div><button>Publish Layouts</button><span>1</span></div>
-                <ul>
-                    <li>13706</li>
-                </ul>
+        <div class="large-12 columns publisher__upload">
+            <div class="large-6 columns">
+                <div class="publisher__box publisher__box-active">
+                    <div class="row publisher__box-header">
+                        <div class="large-6 columns">
+                            <h2 class="publisher__box-title">Product Layouts</h2>
+                        </div>
+                        <div class="large-6 columns">
+                            <div class="publisher__button">
+                                <button class="common__button">Publish Layouts</button>
+                                <span class="publisher__button-count">1</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <ul class="publisher__upload-list large-12 columns">
+                            <li>13706</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="publisher__box">
+                    <div class="row publisher__box-header">
+                        <div class="large-6 columns">
+                            <h2 class="publisher__box-title">Monograms</h2>
+                        </div>
+                        <div class="large-6 columns">
+                            <p class="publisher__upload-status">no changed detected</p>
+                            <div class="publisher__button"><button class="common__button">Publish Monograms</button><span class="publisher__button-count">1</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="publisher__box">
+                    <div class="row publisher__box-header">
+                        <div class="large-6 columns">
+                            <h2 class="publisher__box-title">Pridemarks</h2>
+                        </div>
+                        <div class="large-6 columns">
+                            <p class="publisher__upload-status">no changed detected</p>
+                            <div class="publisher__button"><button class="common__button">Publish Pridemarks</button><span class="publisher__button-count">1</span></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="publisher__upload">
-                <h2>Monograms</h2>
-                <div><button>Publish Monograms</button><span>1</span></div>
-                <ul>
-                    <li>13706</li>
-                </ul>
-            </div>
-            <div class="publisher__upload">
-                <h2>Pridemarks</h2>
-                <div><button>Publish Pridemarks</button><span>1</span></div>
-                <ul>
-                    <li>13706</li>
-                </ul>
-            </div>
-        </div>
-        <div class="large-6 medium-12 columns">
-            <div class="publisher__upload">
-                <h2>Expressions</h2>
-                <div><button>Publish Expressions</button><span>1</span></div>
-                <ul>
-                    <li>13706</li>
-                </ul>
+            <div class="large-6 columns">
+                <div class="publisher__box publisher__box-active">
+                    <div class="row publisher__box-header">
+                        <div class="large-6 columns">
+                            <h2 class="publisher__box-title">Expressions</h2>
+                        </div>
+                        <div class="large-6 columns">
+                            <div class="publisher__button"><button class="common__button">Publish Expressions</button><span class="publisher__button-count">1</span></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <ul class="publisher__upload-list large-12 columns">
+                            <li>13706</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
 <!--         <div class="large-3 medium-4 columns common__sidebar">
