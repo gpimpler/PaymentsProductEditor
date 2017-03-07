7eb8b839bc5fc69b007e170b9e0d54ad671ecdd4
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-07 19:01:44 +0000

    adds migrations for products and styles tables

--- UI/trunk/database/migrations/2017_03_07_170554_create_products_table.php
+++ UI/trunk/database/migrations/2017_03_07_170554_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at')->nullable();
+            $table->integer('id', 11)->unique();
+            $table->string('type');
+            $table->tinyInteger('active')->default(1);
         });
     }
 
@@ -28,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('products');
     }
 }
