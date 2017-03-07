50fa09860194769866dafc7acfc20f5ad10f50b6
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-07 20:00:58 +0000

    adds methods to fetch products and styles

--- UI/trunk/app/Http/Controllers/ProductController.php
+++ UI/trunk/app/Http/Controllers/ProductController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function products()
+    {
+        return Product::get();
+    }
+
+    public function product($id)
+    {
+        return Product::find($id);
+    }
 }
