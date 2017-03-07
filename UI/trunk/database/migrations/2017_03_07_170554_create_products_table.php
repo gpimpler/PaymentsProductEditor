9a2149012c556b4967f05d0f0ac9397dedd3a0e7
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-07 19:28:31 +0000

    adds model and controller for Products and Styles

--- UI/trunk/database/migrations/2017_03_07_170554_create_products_table.php
+++ UI/trunk/database/migrations/2017_03_07_170554_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-            $table->integer('id', 11)->unique();
+            $table->increments('id', 11)->unique();
             $table->string('type');
             $table->tinyInteger('active')->default(1);
         });
