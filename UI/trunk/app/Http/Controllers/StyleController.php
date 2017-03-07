a6b86948015c9e7f13111f0da086b9f1ee4b1570
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-07 20:00:58 +0000

    adds methods to fetch products and styles

--- UI/trunk/app/Http/Controllers/StyleController.php
+++ UI/trunk/app/Http/Controllers/StyleController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Style;
 use Illuminate\Http\Request;
 
 class StyleController extends Controller
 {
-    //
+    public function styles()
+    {
+        return Style::get();
+    }
+
+    public function style($id)
+    {
+        return Style::find($id);
+    }
 }
