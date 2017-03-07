e8c1995eae1d5c27f08ab6afc84acccbd1cd5569
beth3346 <beth3346@8d1f79b8-f5f9-4f70-b126-1f1df5c4ea48>
2017-03-07 19:01:44 +0000

    adds migrations for products and styles tables

---
+++ UI/trunk/database/migrations/2017_03_07_170614_create_styles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStylesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('styles', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->integer('id', 11)->unique()->comment('Unique identifier for the style ids');
+            $table->mediumInteger('product');
+            $table->string('style')->comment('The style id to be used');
+            $table->longText('data')->comment('The json data associated with this style id.');
+            $table->smallInteger('version');
+            $table->timestamp('date')->useCurrent();
+            $table->tinyInteger('active')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('styles');
+    }
+}
